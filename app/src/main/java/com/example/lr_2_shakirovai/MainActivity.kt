a101e46c65de597e5bcbcbfcc0e6d145f87150dd
smakakor <amir.shakirov.2015@mail.ru>
2025-03-13 15:17:43 +0500

    Итог по лр2

--- app/src/main/java/com/example/lr_2_shakirovai/MainActivity.kt
+++ app/src/main/java/com/example/lr_2_shakirovai/MainActivity.kt
@@ -4,13 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lr_2_shakirovai.ui.theme.LR_2_ShakirovAITheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +47,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LR_2_ShakirovAITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
+                    ArtSpaceMain()
                 }
             }
         }
@@ -31,17 +57,118 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ArtSpaceMain() {
+    var result by remember {
+        mutableStateOf(1)
+    }
+    when (result) {
+        1 -> ArtWithTitle(
+            ArtPicture = R.drawable.rm,
+            ArtTitle = stringResource(R.string.volgograd ),
+            ArtName = stringResource(id = R.string.motherlandCalls)
+        )
+
+        2 -> ArtWithTitle(
+            ArtPicture = R.drawable.md,
+            ArtTitle = stringResource(R.string.saintPetersburg),
+            ArtName = stringResource(id = R.string.bronzeHorseman)
+        )
+
+        3 -> ArtWithTitle(
+            ArtPicture = R.drawable.rk,
+            ArtTitle = stringResource(R.string.moscow1937),
+            ArtName = stringResource(id = R.string.workerAndKolhoz)
+        )
+
+        4 -> ArtWithTitle(
+            ArtPicture = R.drawable.pmip,
+            ArtTitle = stringResource(R.string.moscow1818),
+            ArtName = stringResource(id = R.string.mininAndPozharsky)
+        )
+
+        5 -> ArtWithTitle(
+            ArtPicture = R.drawable.pp1,
+            ArtTitle = stringResource(R.string.moscow1997),
+            ArtName = stringResource(id = R.string.peterTheGreat)
+        )
+
+        else -> ArtWithTitle(
+            ArtPicture = R.drawable.rm,
+            ArtTitle = stringResource(R.string.volgograd),
+            ArtName = stringResource(id = R.string.motherlandCalls)
+        )
+    }
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 700.dp)
+    ) {
+        Button(
+            onClick = { if (result > 1) result-- },
+            shape = RoundedCornerShape(50.dp),
+            border = BorderStroke(2.dp, Color.Gray),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 20.dp,
+            )
+        ) {
+            Text(text = "Back")
+        }
+        Button(
+            onClick = { if (result < 5) result++ },
+            shape = RoundedCornerShape(50.dp),
+            border = BorderStroke(2.dp, Color.Gray),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 20.dp
+            )
+        ) {
+            Text(text = "Next")
+        }
+    }
+}
+
+@Composable
+fun ArtWithTitle(ArtPicture : Int, ArtTitle:String, ArtName : String, modifier: Modifier = Modifier) {
+Column (
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+    modifier = Modifier.fillMaxSize()
+) {
+    Image(
+        painter = painterResource(id = ArtPicture),
+        contentDescription = null,
+        modifier = Modifier
+            .padding(20.dp)
+            .height(300.dp)
+            .width(300.dp)
+            .border(border = BorderStroke(2.dp, Color.Gray), shape = RectangleShape)
+            .shadow(elevation = 4.dp)
+            .padding(10.dp)
     )
+    Card (
+        modifier = Modifier
+            .padding(horizontal = 45.dp, vertical = 90.dp)
+    ){
+        Column (modifier = Modifier.padding(16.dp)) {
+            Text(text = ArtName,
+                fontSize = 25.sp,
+                textAlign = TextAlign.Start,
+                lineHeight = 30.sp)
+            Text(
+                text = ArtTitle,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(top = 8.dp)
+            )
+        }
+    }
+}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LR_2_ShakirovAITheme {
-        Greeting("Android")
+        ArtSpaceMain()
     }
 }
